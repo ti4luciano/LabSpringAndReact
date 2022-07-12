91a45feb4e46a90aedc08287ea8aa773854a2ee9
Luciano Alves <ti4luciano@gmail.com>
2022-07-12 20:58:21 -0300

    Starting project

---
+++ backend/src/main/java/com/luciano/spring/LabSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luciano/spring/LabSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
