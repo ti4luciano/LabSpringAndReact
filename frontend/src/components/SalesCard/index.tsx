515cc51226bfe93771cd4b245962375470f2d7d4
Luciano Alves <ti4luciano@gmail.com>
2022-07-12 22:43:23 -0300

    Included datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,5 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
 import NotificationButton from '../NotificationButton'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 import './styles.css'
 
@@ -9,10 +10,20 @@ function SalesCard() {
       <h2 className="sales-title">Vendas</h2>
       <div>
         <div className="form-control-container">
-          <input className="form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
         <div className="form-control-container">
-          <input className="form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
 
