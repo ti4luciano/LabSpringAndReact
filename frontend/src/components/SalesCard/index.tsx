901d83f10efadb05304c0c507ac8d9b2bdc6960f
Luciano Alves <ti4luciano@gmail.com>
2022-07-13 06:47:07 -0300

    Included datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,24 +3,32 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 import './styles.css'
+import { useState } from 'react';
 
 function SalesCard() {
+
+  const min = new Date(new Date().setDate(new Date().getDate() -365));
+  const max = new Date();
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+
   return (
     <div className="card">
       <h2 className="sales-title">Vendas</h2>
       <div>
         <div className="form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => { }}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => { }}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="form-control"
             dateFormat="dd/MM/yyyy"
           />
