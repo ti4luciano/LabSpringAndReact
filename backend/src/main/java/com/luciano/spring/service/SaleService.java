51767db368cb21cefac2b8b7e63234f01d5b6f51
Luciano Alves <ti4luciano@gmail.com>
2022-07-13 21:15:11 -0300

    Basic structure

---
+++ backend/src/main/java/com/luciano/spring/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.luciano.spring.service;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.luciano.spring.entities.Sale;
+import com.luciano.spring.repository.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.now();
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
+	};
+
+}
